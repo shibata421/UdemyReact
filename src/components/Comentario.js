540f650164357a976dfcb6c280e6ff351558c74f
Fernando Shibata <shibata.421@gmail.com>
2020-12-28 16:45:18 -0300

    Construido Componentes

---
+++ src/components/Comentario.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Comentario.css'
+
+// JSX -> HTML dentro do JS
+const Comentario = () => (
+    <div className="Comentario">
+        <div>João:</div>
+        <div>Olá, tudo bem?</div>
+    </div>
+)
+
+export default Comentario
